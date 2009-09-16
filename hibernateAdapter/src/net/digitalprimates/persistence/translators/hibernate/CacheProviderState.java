77d6ee60befead53fd1183b24a6429186e7e969f
martypitt <martypitt@me.com>
2009-09-16 20:40:37 +0000

    Removed compiled files from svn

--- hibernateAdapter/src/net/digitalprimates/persistence/translators/hibernate/CacheProviderState.java
+++
@@ -1,7 +0,0 @@
-package net.digitalprimates.persistence.translators.hibernate;
-
-public enum CacheProviderState {
-	UNKNOWN,
-	CACHED,
-	PENDING
-}
--- hibernateAdapter/src/net/digitalprimates/persistence/translators/hibernate/ITranslationProvider.java
+++
@@ -1,5 +0,0 @@
-package net.digitalprimates.persistence.translators.hibernate;
-
-public interface ITranslationProvider {
-	public boolean willTranslate(Object object);
-}
