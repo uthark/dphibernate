aae25cf6bc7f0746989b00cb0ff11271936842b7
martypitt <martypitt@me.com>
2010-08-26 14:12:12 +0000

    Moved spring dependencies out to exetensions project from core

--- spring-extensions-2.5.6/src/org/dphibernate/persistence/state/AuthenticatedObjectChangeUpdater.java
+++ spring-extensions-2.5.6/src/org/dphibernate/persistence/state/AuthenticatedObjectChangeUpdater.java
@@ -1,5 +1,8 @@
 package org.dphibernate.persistence.state;
 
+import java.util.List;
+import java.util.Set;
+
 import org.dphibernate.serialization.DPHibernateCache;
 import org.hibernate.SessionFactory;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,5 +31,11 @@ public class AuthenticatedObjectChangeUpdater extends ObjectChangeUpdater
 		super(sessionFactory, proxyResolver);
 		setPrincipleProvider(new SpringPrincipalProvider());
 	}
-
+	
+	@Override
+	@Transactional(readOnly=false)
+	public Set<ObjectChangeResult> update(List<ObjectChangeMessage> changeMessages)
+	{
+		return super.update(changeMessages);
+	}
 }
